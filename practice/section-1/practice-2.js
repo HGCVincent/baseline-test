0095fb68bacd9561a0ea85f80a5e9eaaa02ef8a3
VINCENT HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGVI7@OOCL.COM>
2019-07-08 11:55:16 +0800

    vincent's commit

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var result=0;
+  for(var i=0;i<collectionB.length;i++){
+    for(var j=0;j<collectionA.length;j++){
+      if (collectionA[j] = collectionB[i]){
+        result++;
+      }
+    }
+  }
+  return result;
 }
--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var result =new Array() ;
+  for(var i = 0; i<collectionA.length; i++){
+    for (var  ob  in  objectB){
+      if (collectionA[i] = ob){
+        if (!(result[result.length-1] == ob))
+          result.push(ob);
+      }
+    }
+  }
+  return result;
 }
