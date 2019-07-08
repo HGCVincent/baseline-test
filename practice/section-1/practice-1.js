2444711bb0061ead297070775e32110335991986
HGCVincent <44545766+HGCVincent@users.noreply.github.com>
2019-07-08 12:50:55 +0800

    vincent's commit

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -4,8 +4,8 @@ function collectSameElements(collectionA, collectionB) {
   var result =new Array() ;
   for(var i = 0; i<collectionB.length; i++){
     for (var j= 0; j<collectionA.length;j++){
-     if (collectionA[j] = collectionB[i]){
-       if (!(result[result.length-1] == collectionB[i])) 
+     if (collectionA[j] == collectionB[i]){
+       if (result.indexOf(collectionB[i]) == -1)
        result.push(collectionB[i]);
      }
     }
